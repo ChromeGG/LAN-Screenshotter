73e0ca525aba9b4dfade2341e1c4d99944338c03
ChromeGG <imie.nazwisko69.at@gmail.com>
2019-07-09 17:50:48 +0200

    Added icons to buttons

--- src/main/java/com/lanssmaker/controller/ButtonsPaneController.java
+++ src/main/java/com/lanssmaker/controller/ButtonsPaneController.java
@@ -1,4 +1,22 @@
 package com.lanssmaker.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class ButtonsPaneController {
+
+    @FXML
+    private Button dirsButton;
+
+    @FXML
+    private Button screenButton;
+
+    @FXML
+    private Button optionsButton;
+
+    @FXML
+    private void initialize(){
+
+    }
+
 }
